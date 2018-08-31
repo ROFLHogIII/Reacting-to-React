15d597bd623a05a8e795f2917ba0dbe0cfe07083
Richard Garner <richardgarner@mac.com>
2018-08-31 17:39:57 -0500

    lab... complete?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,26 +10,62 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: 'the grey',
+            title: 'the Grey',
             place: 'Mordor',
             text: `How I have longed for Death's gentle caresse`,
+            hasLoaded: false,
         }
+    }
+    ClickToBegin = () => {
+        this.setState({ hasLoaded: true })
+    }
+    ClickToEnd = () => {
+        this.setState({ hasLoaded: false })
     }
     GandalfsTitle = (value) => {
         this.setState({ title: value });
     }
+    Place = (value) => {
+        this.setState({ place: value });
+    }
+    WhaChuSay = (value) => {
+        this.setState({ text: value });
+    }
     render() {
-        return (
-            <div>
-                <input
-                    placeholder="Gandalf's Title"
-                    value={this.state.name}
-                    onChange={(event) => this.GandalfsTitle(event.target.value)}
-                />
-                <h1>Hello {this.props.name}, {this.state.title}! Welcome to {this.props.place}, {this.state.place}!</h1>
-                <h1>{this.props.name}, {this.state.title}: {this.state.text}.</h1>
-            </div>
-        );
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <input
+                        placeholder="Gandalf's Title"
+                        value={this.state.name}
+                        onChange={(event) => this.GandalfsTitle(event.target.value)}
+                    />
+                    <input
+                        placeholder="Place"
+                        value={this.state.name}
+                        onChange={(event) => this.Place(event.target.value)}
+                    />
+                    <input
+                        placeholder="What did you say?"
+                        value={this.state.name}
+                        onChange={(event) => this.WhaChuSay(event.target.value)}
+                    />
+                    <div>
+                        <button onClick={() => this.ClickToEnd()}>Don't click me.</button>
+                    </div>
+                    <h1>Hello {this.props.name + ", " + this.state.title}! Welcome to {this.props.place}: {this.state.place}!</h1>
+                    <h1>{this.props.name + ", " + this.state.title}: {this.state.text}.</h1>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading...</h1>
+                    <button onClick={() => this.ClickToBegin()}>Click to Begin</button>
+                </React.Fragment>
+            )
+        }
+
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-ReactDOM.render(<App name="Gandalf" place="the most lovely place in Middle Earth"/>, document.getElementById(`root`))
+ReactDOM.render(<App name="Gandalf" place="the lovliest place in Middle Earth" />, document.getElementById(`root`))
